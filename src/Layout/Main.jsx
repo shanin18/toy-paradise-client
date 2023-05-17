5dc2a2763f79f1cd8acdc79c8f67c7bc5da5692b
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-17 23:59:08 +0600

    create a navbar and a custom hook

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="container mx-auto">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
