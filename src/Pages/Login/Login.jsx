f58e5f7802f913cbeaee1977eafe0ff66a548085
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-18 20:37:25 +0600

    added login form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="card w-96 mx-auto shadow-2xl mt-24 pt-8">
+      <h2 className="font-bangers text-3xl md:text-4xl text-center">Login</h2>
+      <div className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-archivo">Email</span>
+          </label>
+          <input
+            type="text"
+            placeholder="email"
+            className="input input-bordered font-archivo"
+          />
         </div>
-    );
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text font-archivo">Password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="password"
+            className="input input-bordered font-archivo"
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover font-archivo">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-3">
+          <button className="btn bg-[#01bfff] border-0 font-archivo">Login</button>
+        </div>
+        <small className="font-archivo mt-3">New here? <Link to="/register" className="hover:underline text-[#01bfff]">create an account</Link></small>
+        <div className="divider font-archivo">OR</div>
+        <div className="flex justify-center">
+          <button className="btn btn-ghost btn-circle">
+            <img
+              className="w-10"
+              src="https://i.ibb.co/zXbZ9B9/Google.png"
+              alt="google logo"
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
